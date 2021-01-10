5a8f2fb468255db8d5248cd036985e8226abca02
W.J. Braun <john.braun@ubc.ca>
2021-01-10 04:00:02 +0000

    version 1.56

--- R/BCCIPlot.R
+++ R/BCCIPlot.R
@@ -1,25 +1,29 @@
-BiasVarPlot <- function(data, k1=1, k2=2, h, h2, output=FALSE, g, layout=TRUE) {
+BCCIPlot <-
+function(data, k1=1, k2=2, h, h2, output=FALSE, g, layout=FALSE, incl.biasplot=FALSE, plotdata = TRUE) {
     xy <- data
     bias.out <- LPBias(xy,k1=k1,k2=k2,h=h,h2=h2)
     x <- bias.out$x
     y <- bias.out$y
-    basicCI.UL <- bias.out$basicCI.UL
-    basicCI.LL <- bias.out$basicCI.LL
+    BCCI.UL <- bias.out$BCCI.UL
+    BCCI.LL <- bias.out$BCCI.LL
     biasCI.LL <- bias.out$biasCI.LL
     biasCI.UL <- bias.out$biasCI.UL
     if (layout) {
         oldpar <- par(mfrow=c(2,1), mar=c(4,4,0,1))
         on.exit(par(oldpar))
     }
-    y.range <- range(c(basicCI.UL, basicCI.LL))
-    plot(x, basicCI.LL, ylim=y.range, type="l", ylab=names(xy)[2], xlab=
+    y.range <- range(c(BCCI.UL, BCCI.LL))
+    plot(x, BCCI.LL, ylim=y.range, type="l", ylab=names(xy)[2], xlab=
 names(xy)[1], lty=2)
-    lines(x,y)
-    lines(x, basicCI.UL, lty=2)
+    lines(x,bias.out$yBC)
+    lines(x, BCCI.UL, lty=2)
+    if (plotdata) points(data[,1], data[,2])
     if (!missing(g)) lines(x, g(x), col=2, lwd=2)
-    plot(x, biasCI.LL, ylim=y.range-mean(y), type="l", xlab=names(xy)[1], 
+    if (incl.biasplot) {
+        plot(x, biasCI.LL, ylim=y.range-mean(y), type="l", xlab=names(xy)[1], 
 ylab="bias")
-    lines(x, biasCI.UL)
+        lines(x, biasCI.UL)
+    }
     Bias <- function(x,y,k,numgrid=401,g, h){
          n <- length(x);     a <- min(x);    b <- max(x); # h <- dpill(x,y)
          g0.l1 <- locpoly(x, g(x), bandwidth=h, degree=k, range.x=c(a-h,b+h), gridsize=numgrid)
