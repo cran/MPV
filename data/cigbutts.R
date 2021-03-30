fb90f43365b8f16d41d8da3553e8b16dcfaf7755
W.J. Braun <john.braun@ubc.ca>
2021-03-30 06:20:02 +0000

    version 1.57

--- data/cigbutts.R
+++
@@ -1,5 +0,0 @@
-cigbutts <-
-structure(list(distance = c(200, 400, 600, 800, 1000, 1200, 1400, 
-1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000), count = c(28, 
-15, 10, 11, 9, 4, 1, 1, 2, 1, 2, 1, 1, 0, 0)), .Names = c("distance", 
-"count"), row.names = c(NA, -15L), class = "data.frame")
--- data/stain.R
+++
@@ -1,7 +0,0 @@
-stain <-
-structure(list(treatment = structure(c(5L, 5L, 5L, 2L, 2L, 2L, 
-4L, 4L, 4L, 3L, 3L, 3L, 1L, 1L, 1L), .Label = c("baking soda", 
-"club soda", "cold water", "hot water", "vinegar"), class = "factor"), 
-    response = c(2L, 2L, 2L, 1L, 2L, 0L, 2L, 3L, 2L, 3L, 3L, 
-    4L, 2L, 3L, 3L)), .Names = c("treatment", "response"), row.names = c(NA, 
--15L), class = "data.frame")
